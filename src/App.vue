cfd612e23fdfb3e1bf6f9c95616968241b3af0ad
Kate Grigoryeva <kate23april@yandex.ru>
2025-03-19 20:57:40 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+import AllMovies from './components/AllMovies.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <AllMovies />
+    </div>
+  </header>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AllMovies.vue
@@ -0,0 +1,18 @@
+<script setup>
+import styles from './AllMovies.module.scss'
+console.log('hello')
+</script>
+<template>
+    <div :class="styles.container">Movies</div>
+    <p :class="$style.red">
+    Текст
+  </p>
+</template>
+<style module lang="scss">
+.red {
+  color: red;
+}
+.bold {
+  font-weight: bold;
+}
+</style>
