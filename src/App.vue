9ac3511ccc1803276ab5dd883ccb38eb95d54c4b
Kate Grigoryeva <kate23april@yandex.ru>
2025-03-24 02:24:58 +0300

    add pinia store

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,35 @@
 <script setup>
-import { RouterView } from 'vue-router'
-// import styles from "./App.module.scss"
+// import { onMounted, ref, computed } from 'vue';
+import { RouterView } from 'vue-router';
+import styles from "./App.module.scss";
+import { useMovieStore } from './store';
+
+const movieStore = useMovieStore();
+
+const handleSort = (arg) => {
+	if(arg === 'year') {
+    movieStore.cancelSortByName();
+    movieStore.setSortByYear();
+    movieStore.showSortByYear && sortByYear();
+	} else if (arg === 'name') {
+    movieStore.cancelSortByYear();
+    movieStore.setSortByName();
+    movieStore.showSortByName && sortByName();
+	}
+}
+
+function sortByName() {
+  movieStore.sortedMovies.sort((a, b) => a.title.localeCompare(b.title))
+	// console.log(movieStore.sortedMovies)
+}
+function sortByYear() {
+	movieStore.sortedMovies.sort((a, b) => a.year - b.year)
+	// console.log(movieStore.sortedMovies)
+}
+
+function clearMoviesList() {
+  movieStore.clearStore()
+}
 </script>
 
 <template>
@@ -9,13 +38,40 @@ import { RouterView } from 'vue-router'
   </header>
   <div class="wrapper">
     <main class="main">
+      <nav :class="styles.nav">
+        <template v-if="$route.path === '/'">
+          <h1 :class="styles.navHeader">Фильмы</h1>
+          <div :class="styles.sortTab">
+            <input type="checkbox" 
+              @click="handleSort('name')" 
+              id="nameInpt" 
+              :class="styles.checkInpt">
+            <label for="nameInpt" :class="[styles.inputLabel, { active: movieStore.showSortByName }]">
+              Отсортировать по названию</label>
+            <input type="checkbox" 
+              @click="handleSort('year')" 
+              id="yearInpt" 
+              :class="styles.checkInpt">
+            <label for="yearInpt" :class="[styles.inputLabel, { active: movieStore.showSortByYear }]">
+              Отсортировать по году</label>
+          </div>
+        </template>
+        <template v-else>
+          <div :class="styles.sortTab">
+            <RouterLink :to="{ path: '/' }" :class="styles.return" @click="clearMoviesList()">
+              <span :class="styles.arrow"></span>
+              Назад к списку
+            </RouterLink>
+          </div>
+        </template>
+      </nav>
       <!-- <nav class="nav"></nav> -->
       <RouterView />
     </main>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .header {
   width: 100%;
   height: 100px;
@@ -23,6 +79,8 @@ import { RouterView } from 'vue-router'
   display: flex;
   justify-content: center;
   align-items: center;
+  -webkit-box-shadow: 0px 8px 7px 0px #00000047;
+  -moz-box-shadow: 0px 8px 7px 0px #00000047;
   box-shadow: 0px 8px 7px 0px #00000047;
 }
 .logo {
@@ -51,14 +109,20 @@ import { RouterView } from 'vue-router'
     margin: 0 5px;
   }
 }
-/* .nav {
-    width: 100%;
-    height: 124px;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-end;
-    align-items: flex-start;
-    border-bottom: 1px solid grey;
-    margin-bottom: 32px;
-} */
+
+.active {
+  &::before {
+      content: url(./assets/img/check_box_active.svg);
+      display: block;
+      position: absolute;
+      top: -1px;
+      left: -25px;
+      width: 18px;
+      height: 18px;
+      background-color: transparent;
+  }
+}
+</style>
+
+<style module>
 </style>
--- src/components/AllMovies.vue
+++ src/components/AllMovies.vue
@@ -1,110 +1,41 @@
 <script setup>
-import { onMounted, ref, computed } from 'vue';
+import { onMounted } from 'vue';
 import axios from 'axios';
-import styles from './AllMovies.module.scss';
+// import styles from './AllMovies.module.scss';
 import MovieCard from './MovieCard.vue';
-import { RouterView } from 'vue-router';
 
-const movies = ref([]);
-const sortedMovies = ref([]);
-const showSortByName = ref(false);
-const showSortByYear = ref(false);
+import { useMovieStore } from '@/store';
+const movieStore = useMovieStore();
 
 async function fetchData() { 
-  const response = await axios.get('https://mashroom-movies-api.netlify.app/api/movies') 
-  movies.value = response.data.data.slice(0, 4); 
-  sortedMovies.value = response.data.data.slice(0, 4);
+  const response = await axios.get('https://mashroom-movies-api.netlify.app/api/movies');
+  movieStore.setMovies(response.data.data.slice(0, 4)) 
+  movieStore.setSortedMovies(response.data.data.slice(0, 4)) 
+  // console.log('запрос всех фильмов')
 } 
 
 onMounted(async () => { 
   await fetchData();
-  console.log(movies)
 })
-
-const handleSort = (arg) => {
-	if(arg === 'year') {
-    showSortByName.value = false;
-    compYear.value && sortByYear()
-	} else if (arg === 'name') {
-    showSortByYear.value = false;
-    compName.value && sortByName()
-	}
-}
-
-const compName = computed(() => {return showSortByName})
-const compYear = computed(() => {return showSortByYear})
-
-function sortByName() {
-	sortedMovies.value.sort((a, b) => a.title.localeCompare(b.title))
-	console.log(sortedMovies)
-}
-function sortByYear() {
-	sortedMovies.value.sort((a, b) => a.year - b.year)
-	console.log(sortedMovies)
-}
 </script>
 
 <template>
-  <nav :class="styles.nav">
-    <template v-if="$route.path === '/'">
-      <h1 :class="styles.navHeader">Фильмы</h1>
-      <div :class="styles.sortTab">
-        <input type="checkbox" 
-          v-model="showSortByName" 
-          @click="handleSort('name')" 
-          id="nameInpt" 
-          :class="styles.checkInpt">
-        <label for="nameInpt" :class="[styles.inputLabel, { active: showSortByName }]">
-          Отсортировать по названию</label>
-        <input type="checkbox" 
-          v-model="showSortByYear" 
-          @click="handleSort('year')" 
-          id="yearInpt" 
-          :class="styles.checkInpt">
-        <label for="yearInpt" :class="[styles.inputLabel, { active: showSortByYear }]">
-          Отсортировать по году</label>
-      </div>
-    </template>
-    <template v-else>
-      <div :class="styles.sortTab">
-        <RouterLink :to="{ path: '/' }" :class="styles.return">
-          <span :class="styles.arrow"></span>
-          Назад к списку
-        </RouterLink>
-      </div>
-    </template>
-  </nav>
-  <template v-if="$route.path === '/'">
-    <template v-if="showSortByName || showSortByYear">
+    <template v-if="movieStore.showSortByName || movieStore.showSortByYear">
       <ul>
-        <MovieCard v-for="movie in sortedMovies" :key="movie.id" :movie="movie"/>
+        <MovieCard v-for="movie in movieStore.sortedMovies" :key="movie.id" :movie="movie"/>
       </ul>
     </template>
-    <template v-else-if="movies.length">
+    <template v-else-if="movieStore.movies.length">
       <ul>
-        <MovieCard v-for="movie in movies" :key="movie.id" :movie="movie"/>
+        <MovieCard v-for="movie in movieStore.movies" :key="movie.id" :movie="movie"/>
       </ul>
     </template>
     <p v-else>К сожалению, по вашему запросу ничего не найдено...</p>
-  </template>
-  <template v-else>
+  <!-- </template> -->
+  <!-- <template v-else>
     <RouterView />
-  </template>
+  </template> -->
 </template>
 
 <style module>
-</style>
-<style scoped lang="scss">
-.active {
-  &::before {
-      content: url(../assets/img/check_box_active.svg);
-      display: block;
-      position: absolute;
-      top: -1px;
-      left: -25px;
-      width: 18px;
-      height: 18px;
-      background-color: transparent;
-  }
-}
 </style>
